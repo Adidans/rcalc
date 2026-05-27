182e1587345c3153406858b9396421f3cb884240
Adidans <adrians.uvis@gmail.com>
2026-05-27 23:55:06 +0300

    Implement lexing and command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+use rcalc::Lexer;
+
+#[derive(Parser)]
+#[command(version, about)]
+struct Cli {
+    expr: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    let lexer = Lexer::new(&cli.expr);
+    for token in lexer {
+        match token {
+            Ok(token) => println!("{:?}", token),
+            Err(err) => eprintln!("{err}"),
+        }
+    }
 }
